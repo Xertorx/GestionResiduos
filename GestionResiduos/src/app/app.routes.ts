36f7964c224e3e1b327264672ec79e3f3415ecf4
Juan Loaiza <jloaizap1@ucentral.edu.co>
2025-10-01 23:56:02 -0500

    Se agrega pantalla de reportes

--- GestionResiduos/src/app/app.routes.ts
+++ GestionResiduos/src/app/app.routes.ts
@@ -6,20 +6,22 @@ import { Verify } from './pages/register/verify/verify';
 import { Profile } from './pages/register/profile/profile';
 import { Login } from './pages/login/login';
 import { Calendar } from './pages/calendar/calendar';
+import { Reports } from './pages/reports/reports';
 
 
 export const routes: Routes = [
     {path: '', component: Home, pathMatch: 'full'},
-    {path: 'about_us', component: About},
+    
+    // Redirecciones con slash final
+    {path: 'calendar/', component: Calendar},
+    //Paginas
     {path: 'register', component: Register},
     {path: 'register/verify', component: Verify},
     {path: 'login', component: Login},
     {path: 'register/profile', component: Profile},
     {path: 'calendar', component: Calendar},
-    // Redirecciones con slash final
-    {path: 'calendar/', component: Calendar},
     {path: 'about_us/', component: About},
     {path: 'register/', component: Register},
-    {path: 'login/', component: Login},
+    {path: 'reports', component: Reports},
     {path: '**', redirectTo: ''}
 ];
