59c2fef5dade223be4b88c0ef45292bcb6ba7c3d
Diego A. Garcia M. <garciadiego9602@gmail.com>
2026-04-22 01:25:13 -0500

    correcion e implementacion de HU20 Y HU21, ya sirve el boton editar y eliminar. tambien se agrego subir multiples archivos a un solo formulario

--- GestionResiduos/src/app/shared/components/lucide-icons.ts
+++ GestionResiduos/src/app/shared/components/lucide-icons.ts
@@ -6,6 +6,7 @@ import {
   ThumbsUp, ThumbsDown, Facebook, Twitter, Instagram, Youtube,
   Send, Home, Folder, Settings, BarChart2, Leaf, RefreshCw,
   CircleCheckBig, CircleAlert, XCircle, Key, UserCheck, UserX, Edit, Trash, Plus, Minus, Search, Lock, X, Upload, Inbox,
+  Pencil, Info, File,
 } from 'lucide-angular';
 
 export const LUCIDE_ICONS = {
@@ -15,5 +16,6 @@ export const LUCIDE_ICONS = {
   BookOpen, TrendingUp, Download, FileText, Video, Image,
   ThumbsUp, ThumbsDown, Facebook, Twitter, Instagram, Youtube,
   Send, Home, Folder, Settings, BarChart2, Leaf, RefreshCw,
-CircleCheckBig, CircleAlert, XCircle, Key, UserCheck, UserX, Edit, Trash, Plus, Minus, Search, Lock, X, Upload, Inbox,
+  CircleCheckBig, CircleAlert, XCircle, Key, UserCheck, UserX, Edit, Trash, Plus, Minus, Search, Lock, X, Upload, Inbox,
+  Pencil, Info, File,
 };
